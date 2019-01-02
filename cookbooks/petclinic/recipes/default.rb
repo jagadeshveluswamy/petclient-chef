7c61860b62337ee4e5ed265774d45dde1eb10deb
jagadeshveluswamy <jagadeshveluswamy@gmail.com>
2019-01-02 14:41:59 +0000

    modifying host name in recipe

--- cookbooks/petclinic/recipes/default.rb
+++ cookbooks/petclinic/recipes/default.rb
@@ -11,7 +11,7 @@ tomcat_service "petclinicserver" do
  env_vars [{'CATALINA_BASE' => '/opt/tomcat_petclinicserver_8_0_36/'},{'CATALINA_PID' => '$CATALINA_BASE/bin/tomcat.pid'},{'JAVA_OPTS' => '-Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses=true'}]
 end
 remote_file '/opt/tomcat_petclinicserver_8_0_36/webapps/petclinic.war' do
- source 'http://localhost:18081/artifactory/libs-release-local/org/springframework/samples/spring-petclinic/1.0.0/spring-petclinic-1.0.0.war'
+ source 'http://IE-00001718.ericsson.se:18081/artifactory/libs-release-local/org/springframework/samples/spring-petclinic/1.0.0/spring-petclinic-1.0.0.war'
  owner 'tomcat_petclinicserver'
  group 'tomcat_petclinicserver'
 end
